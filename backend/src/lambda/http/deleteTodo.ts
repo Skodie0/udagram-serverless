7434dae82b1d318978d63b6505fc193f16e277a9
Kodie Samuel Martey <samuelmkodie@gmail.com>
2022-09-07 10:15:57 +0000

    initial commit

---
+++ backend/src/lambda/http/deleteTodo.ts
@@ -0,0 +1,31 @@
+import 'source-map-support/register'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import { deleteTodo } from '../../businessLogic/todos'
+import { getUserId } from '../utils'
+import { createLogger } from '../../utils/logger'
+
+
+    // TODO: Remove a TODO item by id
+
+    const logger = createLogger('DeleteTodo')
+
+    export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+      logger.info('Deleting todo item: ', { event })
+    
+      const userId = getUserId(event)
+      const todoId = event.pathParameters.todoId
+    
+      await deleteTodo(userId, todoId)
+    
+      return {
+        statusCode: 204,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true
+        },
+        body: JSON.stringify({
+          message: `Todo item ${todoId} was deleted successfully!`
+        })
+      }
+    }
+       
